64b32e871bc5177676b5f1276e4f8d16d7e16ffc
GR1BU <108418825+GR1BU@users.noreply.github.com>
2025-07-26 13:07:29 +0300

    Python core 2.0 модуль 3 завдання 2

---
+++ main2.py
@@ -0,0 +1,11 @@
+import random
+
+def get_numbers_ticket(min, max, quantity):
+    if min < 1 or max > 1000 or min > max or quantity > (max - min + 1):
+        return []
+    
+
+    numbers = random.sample(range(min, max + 1), quantity)
+    return sorted(numbers)
+
+print(get_numbers_ticket(1, 1000, 15))
