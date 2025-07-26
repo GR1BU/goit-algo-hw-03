8959c685c23c4dc2371f41e09bd7c4582ace2936
GR1BU <108418825+GR1BU@users.noreply.github.com>
2025-07-26 13:46:42 +0300

    змінений файл python core 2.0 завдання 1

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-def get_days_from_today(date):
-    from datetime import datetime
-    today = datetime.today().date()
-    date = datetime.strptime(date, "%Y-%m-%d").date()
-    days_diff = (today - date).days
-    return (today - date).days
+from datetime import datetime
 
-print("Різниця в днях:", get_days_from_today("2023-01-01"))
+def get_days_from_today(date_str):
+    try:
+        today = datetime.today().date()
+        date = datetime.strptime(date_str, "%Y-%m-%d").date
+        return (today - date).days
 
-    
+    except ValueError:
+        print("неправильний формат дати. Використовуйте YYYY-MM-DD.")
+        return None
+
