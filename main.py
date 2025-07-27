7956cc35074b33a7882b9fa579897afde28ba5e9
GR1BU <108418825+GR1BU@users.noreply.github.com>
2025-07-27 12:12:04 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,17 @@ from datetime import datetime
 def get_days_from_today(date_str):
     try:
         today = datetime.today().date()
-        date = datetime.strptime(date_str, "%Y-%m-%d").date
+        date = datetime.strptime(date_str, "%Y-%m-%d").date()
         return (today - date).days
 
     except ValueError:
         print("неправильний формат дати. Використовуйте YYYY-MM-DD.")
         return None
+    
+
+    
+print("Різниця в днях:", get_days_from_today("2023/01/01"))
+
+print("Різниця в днях:", get_days_from_today("2023-01-01"))
+
 
